ddaa6dabba4ca9f3139568b2369eda1582744221
Toma <tzastrovskaya@gmail.com>
2020-03-23 14:56:56 +0300

    Добавьте файлы проекта.

---
+++ TodoApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<TodoModel> _todoDate;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _todoDate = new BindingList<TodoModel>()
+            {
+            new TodoModel(){Text = "text"},
+            new TodoModel(){Text ="tododo"}
+            };
+            dgTodoList.ItemsSource = _todoDate;
+
+        }
+    }
+}
---
+++ TodoApp/Models/TodoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    class TodoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _IsDone;
+        public bool IsDone
+        {
+            get { return _IsDone; }
+            set { _IsDone = value; }
+        }
+
+
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+    }
+}
