3ce9b4a91bb0de642142f64a5499988e835da672
Toma <tzastrovskaya@gmail.com>
2020-04-02 23:32:41 +0300

    Добавлен Logger

---
+++ TodoApp/Services/Logging/FileLogger.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+
+namespace TodoApp.Services
+{
+    internal class FileLogger : Microsoft.Build.Framework.ILogger 
+    {
+        private string filepath;
+        private static object _loc = new object();
+
+        public LoggerVerbosity Verbosity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Parameters { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+     
+        public FileLogger(string path)
+        {
+            filepath = path;
+        }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(Microsoft.Extensions.Logging.LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(
+            Microsoft.Extensions.Logging.LogLevel logLevel,
+            EventId eventId,
+            TState state,
+            Exception exception,
+            Func<TState, Exception, string> formatte
+            )
+        {
+            if (formatte != null)
+            {
+                lock (_loc)
+                {
+                    File.AppendAllText(
+                        filepath, formatte(state, exception) + Environment.NewLine
+                      
+                        );
+                }
+            }
+        }
+
+        public void Initialize(IEventSource eventSource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ TodoApp/Services/Logging/FileLoggerProvider.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Services
+{
+     internal class FileLoggerProvider : ILoggerProvider
+        // путь, куда будут записываться логи
+        {
+            private string path;
+            // свойство для хранения пути
+            public FileLoggerProvider(string _path)// конструктор для создания провайдера с заданными настройками
+            {
+                this.path = _path;
+            }
+
+            public ILogger CreateLogger(string categoryName) // создаёт  соединение
+            {
+                return FileLogger(path);// возвращает класс, который отвечает за запись логов туда
+            }
+
+            private ILogger FileLogger(string path)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Dispose()// удаляет соединение
+            {
+
+            }
+        }
+}
